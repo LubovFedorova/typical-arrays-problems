5fcff8f283980f68799e4c27b8f034491f066475
Lubov Fedorova <luba.zaverugo@gmail.com>
2021-02-22 16:21:52 +0300

    impl

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  if (arr === undefined || arr.length === 0) {
+        return 0;
+      } else {
+       return arr.reduce((prev,item) => prev < item ? prev : item);
+      }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  if (arr === undefined || arr.length === 0) {
+        return 0;
+      } else {
+      return arr.reduce((prev,item) => prev > item ? prev : item);
+      }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  if (arr === undefined || arr.length === 0) {
+        return 0;
+      } else {
+      return arr.reduce((prev, item) => prev + item, 0) / arr.length;
+      }
 }
